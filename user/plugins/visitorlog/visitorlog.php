173daaa4cb830e70247bfa7a0198563f5d6731c8
Sibiya <shaban532@gmail.com>
2024-05-21 12:39:37 +0330

    Update visitorlog.php

--- user/plugins/visitorlog/visitorlog.php
+++ user/plugins/visitorlog/visitorlog.php
@@ -20,7 +20,25 @@ class VisitorlogPlugin extends Plugin
     protected function save()
     {
 	date_default_timezone_set("Asia/Tehran");
-	$ip = "IP: "  . $_SERVER['REMOTE_ADDR'];
+	## $ip = "IP: "  . $_SERVER['REMOTE_ADDR'];
+	$ip = "";
+	if($_SERVER) {
+	if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
+	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	}elseif(isset($_SERVER['HTTP_CLIENT_IP'])){
+	$ip = $_SERVER['HTTP_CLIENT_IP'];
+	}else{
+	$ip = $_SERVER['REMOTE_ADDR'];
+	}
+	} else {
+	if(getenv('HTTP_X_FORWARDED_FOR')){
+	$ip = getenv('HTTP_X_FORWARDED_FOR');
+	}elseif(getenv('HTTP_CLIENT_IP')){
+	$ip = getenv('HTTP_CLIENT_IP');
+	}else{
+	$ip = getenv('REMOTE_ADDR');
+	}
+	}
 	$time= "Time: " . date("Y/m/d h:i:sa");
 	$day=  date("Ymd");
 	$address =  $_SERVER['REQUEST_URI'];
@@ -49,7 +67,7 @@ class VisitorlogPlugin extends Plugin
 		fwrite($file,$ip ."\n" . $time . "\n" . $address . $referer . "\n" . $user_agent . "\n\n" . $old);
 		fclose($file);
 	
-	$log ='log/ip/' . $_SERVER['REMOTE_ADDR'] . '.log';
+	$log ='log/ip/' . $ip . '.log';
 	$old = '';
 	if ( file_exists($log)){
 		$old = file_get_contents($log);
